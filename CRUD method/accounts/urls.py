c43bbd3f93b6b868f76b20b855828e0071e8c06a
Faraz Khan <farazkhan138@gmail.com>
2020-05-16 20:34:50 +0530

    Update urls.py

--- CRUD method/accounts/urls.py
+++ CRUD method/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('dashboard', views.dashboard, name="dash"),
+    path('dashboard', views.dashboard, name="dashboard"),
     path('register', views.register, name="register"),
     path('upload', views.upload, name="upload"),
     path('contact',views.contact,name="contact")
